98e91762d78cb122a23d6f605181e8aeef6210cb
Jony-007 <habiburrahmanjony4@gmail.com>
2022-01-17 16:21:44 +0600

    Traver_journal done with the help of props and maps

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./card.css";
+// import data from "../../data";
 
-const Card = () => {
-  return <div></div>;
+// import Data from "../../Data";
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <img src={props.item.imageUrl} alt={props.item.title} className="card-img" />
+
+      <div className="card-content">
+        <div className="location">
+          <p className="country">
+            <i className="fas fa-map-marker-alt"></i>{props.item.location}
+          </p>
+
+          <a href={props.item.googleMapsUrl} className="maps-location">View on Google Maps</a>
+
+
+        </div>
+          <h1 className="card-title">{props.item.title}</h1>
+          <p className="card-start-end-date">{props.item.startDate} - {props.item.endDate}</p>
+          <p className = "card-description">{props.item.description}</p>
+
+      </div>
+      
+    </div>
+  );
 };
 
 export default Card;
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./main.css";
+import Card from "../card/Card";
+import Data from "../../Data";
 
 const Main = () => {
-  return <div></div>;
+  const data = Data.map((item) => {
+    return <Card item={item} key={item.id} />;
+  });
+  return <div className="main">{data}</div>;
 };
 
 export default Main;
